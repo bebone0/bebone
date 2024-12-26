25068cdbe8d5d65e0f30699ac30e017dd911bf90
Maksim Jaroslavcevas <36482401+Maksasj@users.noreply.github.com>
2024-12-26 13:30:51 +0200

    replaced maps with unordered_map

--- examples/gfx/opengl/5_opengl_flappy_bird/core/texture_loader.cpp
+++ examples/gfx/opengl/5_opengl_flappy_bird/core/texture_loader.cpp
@@ -2,7 +2,7 @@
 
 namespace game::core {
     std::string TextureLoader::assets_path;
-    std::map<const std::string, std::shared_ptr<GLTexture2D>> TextureLoader::loaded_textures;
+    std::unordered_map<const std::string, std::shared_ptr<GLTexture2D>> TextureLoader::loaded_textures;
 
     void TextureLoader::load_textures(const std::string& path) {
         TextureLoader::assets_path = path;
